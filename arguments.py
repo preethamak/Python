4037195900864a2af70712a95cfe5397a4b438e3
preethamak <167765989+preethamak@users.noreply.github.com>
2024-11-16 18:22:49 +0530

    Add files via upload

---
+++ arguments.py
@@ -0,0 +1,10 @@
+def who_Are_you(name):
+    if name[0]=='n':
+        print(" he is a biggest gandu")
+        exit
+    if name[0]=='p':
+        print("he is a loudu")
+    else:
+        print("you are a loudu")
+name = input("enter your name:")   
+who_Are_you(name)             

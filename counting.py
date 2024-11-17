55785efc11fecdcbebdb4bcb3212c35b24812445
preethamak <167765989+preethamak@users.noreply.github.com>
2024-11-17 17:52:11 +0530

    Add files via upload

---
+++ counting.py
@@ -0,0 +1,9 @@
+for i in range(3):
+    print(i)
+    data= input( ("enter the name"))
+for range  in data:
+    if len(data)<5:
+        print("{data}:thats is less than 5")
+else:
+        print("{data}: thats greater than 5")    
+ 

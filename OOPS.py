b09fc56dd1393e6896f0cef2a7e11c603a9a0bab
preethamak <167765989+preethamak@users.noreply.github.com>
2024-11-25 18:16:16 +0530

    Add files via upload

---
+++ OOPS.py
@@ -0,0 +1,11 @@
+class Computer:
+    a = 20
+    b = 5
+    def add(self):
+        print(self.a + self.b)
+    def sub(self):
+        print(self.a - self.b)
+cal1 = Computer()
+cal2 = Computer()
+cal1.add()
+cal2.sub()
